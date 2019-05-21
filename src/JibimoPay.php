fc2a3be60ce02676ed8ed485f2c94d5b0eafe0d5
JTAG <hesamgholami@yahoo.com>
2019-05-22 02:12:13 +0430

    * Add new Jibimo Extended Pay API

--- src/JibimoPay.php
+++ src/JibimoPay.php
@@ -43,8 +43,41 @@ class JibimoPay
      * @param int $transactionId The ID of a pay money transaction that you requested before.
      * @return bool|string CURL execution result.
      */
-    public function validate(int $transactionId)
+    public function validatePay(int $transactionId)
     {
         return Pay::validatePay($this->baseUrl, $this->token, $transactionId);
+    }
+
+    /**
+     * Using this method you can perform a Jibimo extended pay money transaction to a mobile number and an IBAN which
+     * its owner may or may not be in Jibimo.
+     * @param string $mobileNumber Target mobile number to pay money to.
+     * @param int $amount Amount of money to pay in Toomaans.
+     * @param string $privacy Jibimo privacy level of transaction which can be one of `Public`, `Friend` or `Personal`.
+     * @param string $iban The IBAN (Sheba) number of that bank account which you want to transfer money to. Without
+     * leading `IR`.
+     * @param string|null $description Descriptions of transaction which will be show up in Jibimo.
+     * @param string|null $trackerId Tracker ID to be saved in Jibimo and used later for finding transaction. This value
+     * is optional, but it is highly recommended to provide a unique value for it.
+     * @param string|null $name The first name of the person whom you want to pay to.
+     * @param string|null $family The last name of the person whom you want to pay to.
+     * @return bool|string CURL execution result.
+     */
+    public function extendedPay(string $mobileNumber, int $amount, string $privacy, string $iban, ?string $description,
+                                ?string $trackerId, ?string $name, ?string $family)
+    {
+        return Pay::extendedPay($this->baseUrl, $this->token, $mobileNumber, $amount, $privacy, $iban, $description,
+            $trackerId, $name, $family);
+    }
+
+    /**
+     * After creating an extended pay transaction. You will need to check the status of that transaction using this
+     * method.
+     * @param int $transactionId The ID of an extended pay money transaction that you requested before.
+     * @return bool|string CURL execution result.
+     */
+    public function extendedPayValidate(int $transactionId)
+    {
+        return Pay::validateExtendedPay($this->baseUrl, $this->token, $transactionId);
     }
 }
--- src/api/Pay.php
+++ src/api/Pay.php
@@ -57,4 +57,70 @@ class Pay
 
         return CurlHelper::get("$baseUrl/business/pay/$transactionId", $headers);
     }
+
+
+    /**
+     * This function will be used to directly transfer money from Jibimo to a bank account.
+     * @param string $baseUrl URL of Jibimo API.
+     * @param string $token Jibimo API token.
+     * @param $mobileNumber string Mobile number of a person whom you want to pay to.
+     * @param $amount int Amount of payment in Toomaans.
+     * @param $privacy string Privacy scope which can be one of `Public`, `Friend` or `Personal`.
+     * @param $iban string The IBAN (Sheba) number of that bank account which you want to transfer money to.
+     * @param $description string Transaction description which will be appear in feed.
+     * @param $trackerId string A UUID which will be used as factor id. This field is optional but it is highly
+     * recommended to fill it with a unique value.
+     * @param $name string The first name of the person whom you want to pay to.
+     * @param $family string The last name of the person whom you want to pay to.
+     * @return bool|string CURL execution result.
+     */
+    public static function extendedPay(string $baseUrl, string $token, string $mobileNumber, int $amount, string $privacy,  string $iban,
+                                       ?string $description, ?string $trackerId, ?string $name, ?string $family)
+    {
+
+        $headers = CurlHelper::jsonBearerHeader($token);
+
+        $data = [
+            'mobile_number' => $mobileNumber,
+            'amount' => $amount, // ***NOTE*** This amount is in Toomaans
+            'privacy' => $privacy,
+            'iban' => $iban, // Please keep in mind that we should not include `IR` in IBAN
+        ];
+
+        // Optional fields
+
+        if (isset($description)) {
+            $data['description'] = $description;
+        }
+
+        if (isset($trackerId)) {
+            $data['tracker_id'] = $trackerId;
+        }
+
+        if (isset($name)) {
+            $data['name'] = $name;
+        }
+
+        if (isset($family)) {
+            $data['family'] = $family;
+        }
+
+        return CurlHelper::post("$baseUrl/business/extended-pay", $data, $headers);
+    }
+
+    /**
+     * This function will be used to validate an extended pay transaction in Jibimo.
+     * @param string $baseUrl URL of Jibimo API.
+     * @param string $token Jibimo API token.
+     * @param $transactionId int The ID of an extended pay transaction that you made before.
+     * @return bool|string CURL execution result.
+     */
+    public static function validateExtendedPay(string $baseUrl, string $token, int $transactionId)
+    {
+
+        $headers = CurlHelper::jsonBearerHeader($token);
+
+        return CurlHelper::get("$baseUrl/business/extended-pay/$transactionId", $headers);
+    }
+
 }
