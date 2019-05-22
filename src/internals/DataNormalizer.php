96bcd4f6fd5015ec4d450686d06f460985d87dbe
JTAG <hesamgholami@yahoo.com>
2019-05-22 06:45:58 +0430

    * Add a bare data normalizer class

---
+++ src/internals/DataNormalizer.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace puresoft\jibimo\internals;
+
+
+class DataNormalizer
+{
+    public static function normalizeMobileNumber(string $mobileNumber): string
+    {
+        // TODO: Normalize mobile number here
+    }
+
+    public static function normalizePrivacyLevel(string $privacyLevel): string
+    {
+        // TODO: Normalize privacy level here
+    }
+}
