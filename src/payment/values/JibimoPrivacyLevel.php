99e9d95a5d7a6d8139c0416a90eb7f0d1ba6ac78
JTAG <hesamgholami@yahoo.com>
2019-05-26 05:39:29 +0430

    * Add comments for Jibimo value constants

--- src/payment/values/JibimoPrivacyLevel.php
+++ src/payment/values/JibimoPrivacyLevel.php
@@ -8,7 +8,23 @@ abstract class JibimoPrivacyLevel
 {
     // Jibimo privacy levels for transactions
 
-    const PERSONAL = 'Personal';
+    /**
+     * It means the transaction is only visible between two parties that are involved in it,
+     * meaning payer and payee. So only these two people can see this transaction.
+     */
+    const PERSONAL = 'Personal'; //
+
+    /**
+     * It means the transaction is only visible between two parties that are involved in it *AND* their friends,
+     * meaning payer and payee and Jibimo friends of payer and Jibimo friends of payee.
+     * In this privacy level, the amount of transaction is not visible for people other than payer and payee.
+     */
     const FRIEND = 'Friend';
+
+    /**
+     * Means anyone who is registered in Jibimo can see this transaction.
+     * So it can be a good point for promoting your products in a social media like, type of feed.
+     * In this privacy level, the amount of transaction is not visible for people other than payer and payee.
+     */
     const PUBLIC = 'Public';
 }
--- src/payment/values/JibimoTransactionStatus.php
+++ src/payment/values/JibimoTransactionStatus.php
@@ -8,7 +8,18 @@ abstract class JibimoTransactionStatus
 {
     // Jibimo transaction statuses
 
+    /**
+     * Means one of parties were reject to accept the transaction or there is a problem with the transaction.
+     */
     const REJECTED = 'Rejected';
+
+    /**
+     * This status means the transaction is pending for something else to happen.
+     */
     const PENDING = 'Pending';
+
+    /**
+     * This status means that transaction was successful and everything went cool.
+     */
     const ACCEPTED = 'Accepted';
 }
